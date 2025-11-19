c877eff131780eefdf89506cafdc84df880ab273
Sultana Jahan Tahmina <hscsultana22@gmail.com>
2025-11-19 13:11:26 +0600

    Update ToDoApp.dart

--- ToDoApp.dart
+++ ToDoApp.dart
@@ -1,1 +1,64 @@
 #todoApp
+
+  //Simple todo App in Flutter - single file example (main.dart)
+  //No database: stores todos in memory. you can hide SQLite later
+
+  import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TodoApp());
+}
+
+class TodoApp extends
+  StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner:
+      false,
+      title: 'Todo App',
+      theme: ThemeData(primarySwatch: Coders.indigo),
+      home: const TodoHomePage(),
+      );
+  }
+}
+
+class TodoHomePage extends 
+  StatefulWidget {
+  const TodoHomePage({super.key});
+
+  @override
+  State<TodoHomePage> createState() => _TodoHomePageState();
+}
+
+class _TodoHomePageState extends State<TodoHomePage> {
+  final TextEditingController _controller = TextEditingController();
+  final List<Map<String, dynamic>> _todos = [];
+
+  void _addTodo() {
+    final text = _controller.text.trim();
+    if (text.isEmpty) return;
+
+    setState(() {
+      _todos.add({
+        'title': text,
+        'done': false,
+      });
+    });
+
+    _controller.clear();
+  }
+
+  void _toggleDone(int index) {
+    setState(() {
+      todos[index]['done'];
+    });
+  }
+
+  void _removeTodo(int index) {
+    setStae(() {
+      _todas.removeAt(index);
+    });
+  }
